21096266ab33236e7e5b886cb547ca66fd138325
o2babysna <saneequaye006@st.ug.edu.gh>
2025-08-09 14:04:58 +0000

    INTERFACES.cs

---
+++ INTERFACES.cs
@@ -0,0 +1,20 @@
+interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
