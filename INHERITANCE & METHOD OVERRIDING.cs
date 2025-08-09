996639a1d934e75acf8f6e31b0601731ae02f738
o2babysna <saneequaye006@st.ug.edu.gh>
2025-08-09 14:05:26 +0000

    INHERITANCE & METHOD OVERRIDING.cs

---
+++ INHERITANCE & METHOD OVERRIDING.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Assignment2
+{
+    // ---------- 1. INHERITANCE & METHOD OVERRIDING ----------
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
