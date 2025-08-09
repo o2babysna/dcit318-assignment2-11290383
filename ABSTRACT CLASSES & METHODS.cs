046344f3984366e1cfd768c7e51ea203711cf5ff
o2babysna <saneequaye006@st.ug.edu.gh>
2025-08-09 14:05:49 +0000

    ABSTRACT CLASSES & METHODS.cs

---
+++ ABSTRACT CLASSES & METHODS.cs
@@ -0,0 +1,37 @@
+
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
